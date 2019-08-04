12c1b98d181defe1b3db9a6e912b4999e05728a8
narayanshivansh49 <narayanshivansh49@gmail.com>
2019-08-04 23:40:26 +0530

    changed index.html

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,27 @@ pragma solidity >0.4.0 <0.6.0;
 
 contract Election {
 
-    string public candidate;
-    //Constructor
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // mapping of id of candidate with there id
+    mapping(uint=> Candidate) public candidates;
+
+    //store candidate count
+    uint public candidatesCount;
+
+    //constructor
     constructor () public {
-        candidate = "Shivansh";
+
+        addCandidate("Shivansh");
+        addCandidate("Hardik");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
     }
 }
